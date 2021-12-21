6319537dcda5a5dc238051a58d065a3bcc52d24a
timothyshen <timothyshen1@hotmail.com>
2021-12-21 16:42:06 +0000

    Several changes - Added Login module

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,44 @@
 <template>
   <div class="about">
+    <el-row class="main_body">
+      <h1>Sign in</h1>
+      <el-col :span="6" :offset="6">
+        <el-form :model="loginForm">
+          <el-form-item>
+            <div class="login_text">
+              Username
+            </div>
+            <el-input type="text" v-model="loginForm.username">username</el-input>
+          </el-form-item>
+          <el-form-item>
+            <div class="login_text">
+              password
+            </div>
+            <el-input type="password" v-model="loginForm.password">password</el-input>
+          </el-form-item>
+          <el-form-item>
+            <div>
+              <el-link class="forget_link" type="primary">Forget?</el-link>
+            </div>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="isLogin">Login</el-button>
+            <router-link :to="'/register'" target = _blank><el-button type="primary">register</el-button></router-link>
+          </el-form-item>
+        </el-form>
+      </el-col>
+      <el-col :span="6">
+        <div class="button_wrap">
+          <el-button class="selfButton" type="primary" icon="" size="medium"><span>Google login</span></el-button>
+        </div>
+        <div class="button_wrap">
+          <el-button class="selfButton" type="primary" icon="el-icon-google"><span>Facebook Login</span></el-button>
+        </div>
+        <div class="button_wrap">
+          <el-button class="selfButton" type="primary" icon="el-icon-google"><span>Twitter Login</span></el-button>
+        </div>
+      </el-col>
+    </el-row>
     <form @submit.prevent="toggleLogin">
       <label>Username:</label>
       <input v-model="username" name="username">
